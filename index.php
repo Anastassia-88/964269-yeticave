4ad6fd595d5a39a80860bf18774f2b9cbf0f872c
Anastassia-88 <anastassia.russak@gmail.com>
2019-02-03 18:47:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,23 +24,20 @@ $user_name = 'Anastassia'; // укажите здесь ваше имя
             <input class="main-header__search-btn" type="submit" name="find" value="Найти">
         </form>
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
-
         <nav class="user-menu">
-        <?php if($is_auth==1): ?>
-<div class="user-menu__logged">
-    <p>
-        <?php print($user_name); ?>
-    </p>
-  </div>
-<?php else: ?>
-            <ul class="user-menu__list">
-    <li class="user-menu__item">
-      <a href="#">Регистрация</a>
-    </li>
-    <li class="user-menu__item">
-      <a href="#">Вход</a>
-    </li>
-  </ul>
+            <?php if($is_auth): ?>
+                <div class="user-menu__logged">
+                    <p><?php print($user_name); ?></p>
+                </div>
+            <?php else: ?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                        <a href="#">Регистрация</a>
+                    </li>
+                    <li class="user-menu__item">
+                        <a href="#">Вход</a>
+                    </li>
+                </ul>
             <?php endif; ?>
         </nav>
     </div>
