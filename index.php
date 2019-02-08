0f69af79e05664666da51f989d2efa29b8343f28
Anastassia-88 <anastassia.russak@gmail.com>
2019-02-08 10:23:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -101,18 +101,18 @@ $goods = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach ($goods as $product_feature => $feature_definition): ?>
+            <?php foreach ($goods as $lot): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?=$feature_definition['image']; ?>" width="350" height="260" alt="">
+                        <img src="<?=$lot['image']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$feature_definition['category']; ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$feature_definition['name']; ?></a></h3>
+                        <span class="lot__category"><?=$lot['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['name']; ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$feature_definition['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
