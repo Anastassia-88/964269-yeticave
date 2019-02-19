346b14d26be8b4f54d7726275887e44d70a11604
Anastassia-88 <anastassia.russak@gmail.com>
2019-02-19 19:36:34 +0100

    New queries.sql

--- templates/index.php
+++ templates/index.php
@@ -4,9 +4,9 @@
         горнолыжное снаряжение.</p>
     <ul class="promo__list">
         <!--заполните этот список из массива категорий-->
-        <?php foreach ($categories as $category_name): ?>
+        <?php foreach ($categories as $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $category_name; ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $category['name']; ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
