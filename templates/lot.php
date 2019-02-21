62824568b1e034e096f2002038690eb8cab88644
Anastassia Russak <46761351+Anastassia-88@users.noreply.github.com>
2019-02-21 11:48:51 +0100

    Update lot.php

--- templates/lot.php
+++ templates/lot.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>DC Ply Mens 2016/2017 Snowboard</title>
+    <title><?phpDC Ply Mens 2016/2017 Snowboard</title>
     <link href="../css/normalize.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
 </head>
@@ -37,45 +37,23 @@
     <main>
         <nav class="nav">
             <ul class="nav__list container">
-                <li class="nav__item">
-                    <a href="all-lots.html">Доски и лыжи</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Крепления</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Ботинки</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Одежда</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Инструменты</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Разное</a>
-                </li>
+                <?php foreach ($categories as $category); ?>
+                    <li class="nav__item">
+                        <a href="all-lots.html"><?= htmlspecialchars($category['name']); ?></a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </nav>
         <section class="lot-item container">
-            <?php foreach ($lot as $lot_feature): ?>
+            <?php foreach ($lot as $lot): ?>
             <h2><?= htmlspecialchars($lot['name']); ?></h2>
             <div class="lot-item__content">
                 <div class="lot-item__left">
-                    <div class="lot-item__image">
-                        <img src="img/lot-image.jpg" width="730" height="548" alt="Сноуборд">
+                    <div class="lot-item__image"><?= htmlspecialchars($lot['image']); ?>
+                        <img src="<?= htmlspecialchars($lot['image']); ?>" width="730" height="548" alt="<?= htmlspecialchars($lot['name']); ?>">
                     </div>
-                    <p class="lot-item__category">Категория: <span>Доски и лыжи</span></p>
-                    <p class="lot-item__description">Легкий маневренный сноуборд, готовый дать жару в любом парке, растопив
-                        снег
-                        мощным щелчкоми четкими дугами. Стекловолокно Bi-Ax, уложенное в двух направлениях, наделяет этот
-                        снаряд
-                        отличной гибкостью и отзывчивостью, а симметричная геометрия в сочетании с классическим прогибом
-                        кэмбер
-                        позволит уверенно держать высокие скорости. А если к концу катального дня сил совсем не останется,
-                        просто
-                        посмотрите на Вашу доску и улыбнитесь, крутая графика от Шона Кливера еще никого не оставляла
-                        равнодушным.</p>
+                    <p class="lot-item__category">Категория: <span><?= htmlspecialchars($lot['category']); ?></span></p>
+                    <p class="lot-item__description"><?= htmlspecialchars($lot['description']); ?></p>
                 </div>
                 <div class="lot-item__right">
                     <div class="lot-item__state">
@@ -166,24 +144,11 @@
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+            <?php foreach ($categories as $category); ?>
+                <li class="nav__item">
+                    <a href="all-lots.html"><?= $category['name']; ?></a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
