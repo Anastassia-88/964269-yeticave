b2776a26c86bea54b39512f91b13e2ca3ad342e2
Anastassia Russak <46761351+Anastassia-88@users.noreply.github.com>
2019-02-28 16:22:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $layout_content = include_template('layout.php', [
     'content' => $page_content,
     'categories' => $categories,
     'username' => $_SESSION['user']['name'],
-    'title' => 'Главная'
+    'title' => 'YetiCave. Главная'
     ]);
 
 print($layout_content);
